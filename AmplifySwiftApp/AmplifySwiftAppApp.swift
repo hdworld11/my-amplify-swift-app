9a5260e7aa96c07c14e0cfa9a6fce2550f91589d
harshita91 <macster2.0@harshitas-mbp.myfiosgateway.com>
2022-11-19 12:45:57 -0500

    adding project files

---
+++ AmplifySwiftApp/AmplifySwiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmplifySwiftAppApp.swift
+//  AmplifySwiftApp
+//
+//  Created by Harshita Daddala on 11/19/22.
+//
+
+import SwiftUI
+
+@main
+struct AmplifySwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
